9748a4274055887e6a98792dd1170ab3d6cc0b60
hav250p-cmyk <hav250p@gmail.com>
2025-11-22 00:44:27 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: '#eff6ff',
-          500: '#3b82f6',
-          600: '#2563eb',
-          900: '#1e3a8a'
-        }
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
